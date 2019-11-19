607c178e901d2c482ee5277a5baa8a79af656f49
natecoopercmu <57236700+natecoopercmu@users.noreply.github.com>
2019-11-19 11:42:10 -0500

    Add files via upload
    Not sure if this is correct, but take a look. I'm assuming the cost function we are trying to minimize is f(x) := max(ratio(xi)) based on what Grant has made already. This search algorithm takes a free vertex and tries to minimize its ratio by moving in 1 of 6 possible directions. The 7th is just leaving the free vertex where it is.

---
+++ Backtracking_linesearch.m
@@ -0,0 +1,33 @@
+f = @(ratio) max(ratio); %Returns the worst quality of all the vertices
+%This is our cost function
+
+function ratio = getratio(x0,x1,x2)
+    faceVert = [x0;x1;x2];
+    if i == 43025;
+        temp = 1;
+    end
+    inR = inradius(faceVert);
+    ciR = circumradius(faceVert);
+    ratio = ciR/inR;
+end
+
+function x_min = backtrack(x,alpha)
+    %x is a free vertex, alpha is step size
+
+    dir = [1 0 0; 0 1 0; 0 0 1; -1 0 0; 0 -1 0; 0 0 -1; 0 0 0];
+    %Normal directions in 3D space
+    
+    x_min = 0; %Initialized x_min
+    
+    for i = 1:7 %For each normal direction...
+        
+        x_k = dir(i) + x; %find a new vertex that minimizes cost function        
+        
+        x_kratio = getratio(x_k(1),x_k(2),x_k(3)); %get ratio to determine new quality
+        
+        if ( x_kratio < getratio(x(1),x(2),x(3)) ) 
+            x_min = x_k;
+        end % get the new vertex the minimizes ratio
+    end
+    
+end
