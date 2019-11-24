d129731883f3b4c4385786b5552e91a004ffbdb5
GrantZheng86 <33077268+GrantZheng86@users.noreply.github.com>
2019-11-24 16:23:45 -0500

    Update STL_quality.m
    Experimenting on node validity

--- STL_quality.m
+++ STL_quality.m
@@ -10,6 +10,32 @@ close all
 
 % read faces and vertices of the STL file, F is for faces, and  V is for
 % vertices
+%% This Section is testing code for validity checking: The element is 
+% described by vertix (0,0), (-2, 0), and (-1,1)
+x = 0;
+y = 0;
+
+x1 = -1;
+y1 = 1;
+
+x2 = -2;
+y2 = 0;
+
+J_0 = [x1-x x2-x; y1-y y2-y];
+det(J_0)
+
+% Change x and y to to somewhere outside
+x = -2;
+y = 1;
+J_1 = [x1-x x2-x; y1-y y2-y];
+det(J_1)
+
+% Change x and y to be somewhere inside
+x = -0.5;
+y = 0.5;
+J_2 = [x1-x x2-x; y1-y y2-y];
+det(J_2)
+%%
 [F, V] = stlread('OLYMPUS XZ-2.STL'); 
 
 % number of faces
