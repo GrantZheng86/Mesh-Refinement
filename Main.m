c8f0f270dcf07aa12f26947383e775f972b2ff5e
Theodore Lee <lee7670@gmail.com>
2019-12-01 16:07:08 -0500

    Main implementation, still bugged

--- Main.m
+++ Main.m
@@ -3,7 +3,7 @@ clear variables
 close all
 
 %% reading the STL file, remove duplicated vertices and find free vertices
-[F, V] = stlread('Circle2d_norepeat.stl'); 
+[F, V] = stlread('InnerOuterBoundaryMesh2d.stl'); 
 [F,V] = removeDuplicateVertices(F,V);
 freeVerts = findFreeVertices(F,V);
 freeV_l = length(freeVerts);
@@ -14,7 +14,7 @@ f_curr = f_init;
 f_des = 0.3;
 count = 0;
 
-alpha = 0.2;
+alpha = 1;
 tol = 1e-7;
 gamma = 0.9;
 delta = 1.5;
@@ -24,6 +24,31 @@ while (f_curr - f_init) < 0.95 * (f_des - f_init) && count < 5
     for i = 1:freeV_l
         currV = freeVerts(i);
         xi_new = explore(F,currVAll,alpha,currV); 
-        
-        
+        while (~isValidMove(F, currVAll, currV, xi_new))
+            %get direction
+            dir = xi_new - currVAll(currV,:);
+            %multiply by backtracking linesearch parameter
+            dir = dir*gamma;
+            %reassign xi_new
+            xi_new = currVAll(currV,:)+dir;
+        end
+        newVall = currVAll;
+        newVall(currV,:) = xi_new;
+        if (meshQuality(F,newVall) > meshQuality(F, currVAll))
+            currVAll = newVall;
+            alpha = alpha*delta;
+        end
+    end
+    f_new = meshQuality(F, currVAll)
+    if(f_new - f_curr < 1e-15)
+        count = count + 1;
+    end
+    f_curr = f_new;
+end
 
+TR = triangulation(F,currVAll);
+TR2 = triangulation(F, V);
+figure(1)
+triplot(TR)
+figure(2)
+triplot(TR2)
--- isValidMove.m
+++ isValidMove.m
@@ -1,10 +1,41 @@
 function toReturn = isValidMove(F,V,Vi,Vnew)
     l = length(F);
+    VnewAll=V;
+    VnewAll(Vi,:)=Vnew;
     for i = 1:l
         if (F(i,1) == Vi || F(i,2) == Vi || F(i,3) == Vi)
-            % Check Jacobian
-            
-        
+            if(F(i,1) == Vi)
+                V1original = V(F(i,1),:);
+                V2original = V(F(i,2),:);
+                V3original = V(F(i,3),:);
+                V1new = VnewAll(F(i,1),:);
+                V2new = VnewAll(F(i,2),:);
+                V3new = VnewAll(F(i,3),:);
+            elseif(F(i,2) ==  Vi) 
+                V2original = V(F(i,1),:);
+                V1original = V(F(i,2),:);
+                V3original = V(F(i,3),:);
+                V2new = VnewAll(F(i,1),:);
+                V1new = VnewAll(F(i,2),:);
+                V3new = VnewAll(F(i,3),:);
+            else
+                V3original = V(F(i,1),:);
+                V2original = V(F(i,2),:);
+                V1original = V(F(i,3),:);
+                V3new = VnewAll(F(i,1),:);
+                V2new = VnewAll(F(i,2),:);
+                V1new = VnewAll(F(i,3),:);
+            end
+            J_original = [V3original(1)-V1original(1) V2original(1)-V1original(1);V3original(2)-V1original(2) V2original(2)-V1original(2)];
+            J_new = [V3new(1)-V1new(1) V2new(1)-V1new(1);V3new(2)-V1new(2) V2new(2)-V1new(2)];
+            valid=det(J_original)*det(J_new);
+            if(valid < 0)
+                toReturn = false;
+                return;
+            end
+        end
     end
+    toReturn = true;
+    return;
 end
 
